00ac322e7eef3c2badff378ccb4ee4680e70ad57
brstee <98113028+brstee@users.noreply.github.com>
2024-08-20 23:55:13 +0700

    Update setWebhook.php

--- setWebhook.php
+++ setWebhook.php
@@ -1,7 +1,7 @@
 <?php
 
-$token = "5271273866:AAGATshg5hEi5y6GYs4RQyNHEcQJpafaE20";
-$webhookURL = "https://aliastone.com/bot_tg/bot.php";
+$token = "Token_Của_Telebot";
+$webhookURL = "https://đường dẫn vào  file/bot.php";
 $apiURL = "https://api.telegram.org/bot$token/";
 
 $response = file_get_contents($apiURL . "setWebhook?url=" . $webhookURL);
